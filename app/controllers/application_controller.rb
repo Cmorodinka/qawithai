6327fc8ee8154716b87b62106fb2f8bd34dd796c
Cmorodinka <tatyanakosachova@gmail.com>
2023-04-17 22:40:30 -0300

    add locale files, translate left menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+      I18n.locale = params[:locale]
+    elsif cookies[:locale]
+      I18n.locale = cookies[:locale]
+    else
+      I18n.locale = I18n.default_locale
+    end
+  end
+  
 end
